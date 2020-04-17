a9607816383775da5a77cfa5787e311f0dbd7879
pmoreira1 <pedrodavidmoreira@gmail.com>
2020-04-17 11:51:35 +0200

    Final Project RC

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# README
+<img src="https://bit.ly/2VnXWr2" alt="Ironhack Logo" width="100"/>
+
+# Final Project
+
+**The demand level of travellers in data**
+
+*Pedro Moreira*
+
+*Data Analytics, Amsterdam 2020*
+
+## Content
+- [Project Description](#project-description)
+- [Rules](#rules)
+- [Workflow](#workflow)
+- [Organization](#organization)
+- [Links](#links)
+
+## Project Description
+This project goal is to categorize travellers based on their country of origin. This categorization is based on reviews grabbed (via .CSV, API and web scrapping) from booking.com, expedia and yelp.
+
+After a deep analysis of the data grabbed a model will be created to predict a match level of the traveller to the business.
+
+## Data Pipeline
+<img src="https://drive.google.com/open?id=162Heb4EuZ6DcV0PUhXNGiobYvWJ04Ha-" alt="Project Workflow">
+
+## Organization
+```
+FINAL-PROJECT
+│   .gitignore
+│   dataset_importer.py
+│   get_match_level.py
+│   get_recommendations.py
+│   Model decision.ipynb
+│   model_creation.py
+│   Presentation.twb
+│   README.md
+│   Recommendations.twb
+│   yelp_business_grabber.py
+│   yelp_review_grabber.py
+├───data
+│       countries_with_codes.csv
+│       hotel_clean.csv
+│       mysql_reviews.csv
+├───functions
+│       db_class.py
+│       functions.py
+├───models
+│       108
+│       14
+│       157
+│       196
+│       216
+│       234
+│       235
+│       236
+│       41
+│       84
+├───results
+│       recommendations.csv
+
+```
+## Links
+
+[Repository](https://github.com/pmoreira1/final-project)
+
+[Slides](https://docs.google.com/presentation/d/1j0eeq0fz75578fAVJXVEYlf8t-NFS1X-Zo07Bfz8IWA/edit?usp=sharing)
+
--- get_recommendations.py
+++ get_recommendations.py
@@ -8,23 +8,25 @@ pd.set_option('mode.chained_assignment', None)
 # print('Argument List:', str(sys.argv))
 # to be launched as get_recommendations.py category nationality destination
 
-category = sys.argv[1]
-nationality = sys.argv[2]
-destination = sys.argv[3]
+categories = [1, 2]
+nationality = sys.argv[1]
+destination = sys.argv[2]
 
 bins = [0, 0.80, 0.90, 1]
 labels = [1, 2, 3]
 
 loaded_model = pickle.load(open('models/'+str(nationality), 'rb'))
-
-hotels = pd.DataFrame(fc.db.select(query="SELECT * from business where category='"+str(category)+"' and country='"+str(destination)+"'", all=True))
-hotels_to_predict = hotels[['country', 'rating_cal']]
-hotels_to_predict['b_review'] = pd.cut(hotels_to_predict['rating_cal'], bins, labels=labels)
-hotels_to_predict.drop('rating_cal', axis=1, inplace=True)
-hotels['match_level'] = loaded_model.predict(hotels_to_predict)
-hotels.sort_values(by=['match_level', 'rating', 'review_count'], ascending=False, inplace=True)
-
-result = hotels[['business', 'lat', 'long', 'match_level', 'rating', 'review_count']].head(10).reset_index(drop=True)
-result.to_csv('results/result.csv')
-print(result)
+final_result = pd.DataFrame()
+for c in categories:
+    business = pd.DataFrame(fc.db.select(query="SELECT * from business where category='"+str(c)+"' and country='"+str(destination)+"'", all=True))
+    business_to_predict = business[['country', 'rating_cal']]
+    business_to_predict['b_review'] = pd.cut(business_to_predict['rating_cal'], bins, labels=labels)
+    business_to_predict.drop('rating_cal', axis=1, inplace=True)
+    business['match_level'] = loaded_model.predict(business_to_predict)
+    business.sort_values(by=['match_level', 'rating', 'review_count'], ascending=False, inplace=True)
+    result = business[['idbusiness', 'business', 'category', 'lat', 'long', 'match_level', 'rating_cal', 'review_count']][:10].reset_index(drop=True)
+    final_result = final_result.append(result)
+final_result.reset_index(drop=True)
+final_result.to_csv('results/recommendations.csv')
+print(final_result)
 # print(15)
